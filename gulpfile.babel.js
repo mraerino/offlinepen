e934207ff66b15f59b903e93cb1ccbe9e5256acb
mraerino <melodie124@gmail.com>
2017-09-23 02:44:13 +0200

    :hammer: Use cross-origin messaging because of same-origin policy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,7 @@ const dist = 'build/default';
 
 gulp.task('copy-extras', () => gulp.src([
     "preview-worker.js",
-    "node_modules/dexie/dist/dexie.js",
+    "node_modules/dexie/dist/dexie.min.js",
     "bower_components/ace-builds/src-min-noconflict/{theme-,mode-,worker-}*.js"
   ], { base: '.' }).pipe(gulp.dest(dist))
 );
--- preview-worker.js
+++ preview-worker.js
@@ -1,41 +1,15 @@
-self.importScripts('node_modules/dexie/dist/dexie.js');
+self.importScripts('node_modules/dexie/dist/dexie.min.js');
 
 var db;
 function getDb() {
   if (!db) {
-    db = new Dexie('offlinepen');
+    db = new Dexie('offlinepen_preview');
     db.version(1).stores({
-      pens: 'id, name, updatedAt',
-      files: 'id, pen_id, name'
+      pen_content: 'id'
     });
   }
   return db;
 }
-
-const indent = (text, size) => text.split('\n').join(`\n${' '.repeat(size*2)}`);
-
-const previewTemplate = ({ html = "", css = "", js = "", title = "OfflinePen" }) =>
-`<!doctype html>
-
-<html>
-<head>
-  <meta charset="utf-8">
-
-  <title>${title} - OfflinePen</title>
-  <meta name="generator" content="OfflinePen">
-
-  <style>
-    ${indent(css,2)}
-  </style>
-</head>
-<body>
-  ${indent(html,1)}
-
-  <script>
-    ${indent(js,2)}
-  <\/script>
-</body>
-</html>`;
 
 const errorTemplate = message =>
 `<!doctype html>
@@ -69,23 +43,14 @@ self.addEventListener('fetch', function(event) {
   if(['p.offlinepen.space', 'localhost'].indexOf(url.hostname) !== -1 && url.pathname.startsWith('/s/')) {
     const db = getDb();
     const id = url.pathname.split('/')[2];
-    const props = {};
     event.respondWith(
-      db.pens.get(id)
+      db.pen_content.get(id)
         .then(pen => {
           if(!pen) {
             throw new Error("Pen not found!");
           }
-          props.title = pen.name;
+          return pen.content;
         })
-        .then(() => db.files.where('pen_id').equals(id).each(file => {
-          const [name, type] = file.name.split('.');
-          if(name !== 'index') {
-            return;
-          }
-          props[type] = file.content;
-        }))
-        .then(() => previewTemplate(props))
         .catch(e => errorTemplate(e.message))
         .then(html => new Response(html, { headers: { 'Content-Type': 'text/html;charset=UTF-8' }}))
     );
